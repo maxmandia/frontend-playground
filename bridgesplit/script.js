a9fcb7daf64cebf7086c57c9dcd94fda56132b38
Max Mandia <mmandia511@gmail.com>
2022-10-06 21:27:26 -0400

    saving fade in work

--- bridgesplit/script.js
+++ bridgesplit/script.js
@@ -1,3 +1,4 @@
+const text = document.querySelector(".typewriter");
 const messageArray = [
   "real estate",
   "commercial loans",
@@ -9,7 +10,6 @@ let textPosition2 = 0;
 let textPosition3 = 0;
 let textPosition4 = 0;
 let speed = 150;
-const text = document.querySelector(".typewriter");
 
 (function typewriter() {
   if (textPosition1++ <= messageArray[0].length) {
@@ -17,28 +17,50 @@ const text = document.querySelector(".typewriter");
       messageArray[0].substring(0, textPosition1) + "<span>\u25ae</span>";
     setTimeout(typewriter, speed);
   } else if (textPosition2++ <= messageArray[1].length) {
-    console.log(textPosition2);
     text.innerHTML =
       messageArray[1].substring(0, textPosition2) + "<span>\u25ae</span>";
     setTimeout(typewriter, speed);
   } else if (textPosition3++ <= messageArray[2].length) {
-    console.log(textPosition3);
     text.innerHTML =
       messageArray[2].substring(0, textPosition3) + "<span>\u25ae</span>";
     setTimeout(typewriter, 250);
   } else if (textPosition4++ <= messageArray[3].length) {
-    console.log(textPosition4);
     text.classList.add("color-change");
     text.innerHTML =
       messageArray[3].substring(0, textPosition4) + "<span>\u25ae</span>";
     if (textPosition4 - 1 === messageArray[3].length) {
       const span = document.querySelector("span");
       text.innerHTML = messageArray[3].substring(0, textPosition4);
-      console.log("done");
       span.classList.add("stop-animation");
     }
     setTimeout(typewriter, speed);
   }
 })();
 
-// typewriter();
+// fade in
+
+const faders = document.querySelectorAll(".fade-in");
+const options = {
+  threshold: 0.5,
+  //   rootMargin: "0px 0px -200px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      console.log("yo");
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+options);
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
