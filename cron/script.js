12476871d9656bbdeb5f5c2e4a8b05809a5a02a0
Max Mandia <mmandia511@gmail.com>
2022-10-09 12:48:49 -0400

    completed cron work

--- cron/script.js
+++ cron/script.js
@@ -1,4 +1,18 @@
-const btn = document.querySelector(".menu");
+const hamburger = document.querySelector(".menu");
 const menu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
+const x = document.querySelector(".x");
 
-btn.addEventListener("click", () => {});
+hamburger.addEventListener("click", () => {
+  menu.style.display = "block";
+  body.style.overflow = "hidden";
+  hamburger.style.display = "none";
+  x.style.display = "block";
+});
+
+x.addEventListener("click", () => {
+  menu.style.display = "none";
+  body.style.overflow = "scroll";
+  hamburger.style.display = "block";
+  x.style.display = "none";
+});
