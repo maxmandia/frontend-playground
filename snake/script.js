5fbcf09bc17d250db041deaef1bfbbacf07de6f5
Max Mandia <mmandia511@gmail.com>
2022-10-12 19:45:43 -0400

    saving snake work

--- snake/script.js
+++ snake/script.js
@@ -2,6 +2,7 @@ const cells = document.querySelectorAll(".cell");
 let cellArr = [];
 cells.forEach((cell) => cellArr.push(cell));
 let intervalID;
+let delay = 100;
 
 (function assignRandomCell() {
   let randomCell = Math.floor(Math.random() * cellArr.length);
@@ -10,28 +11,26 @@ let intervalID;
 
 let startingSnakeSize = [0, 1, 2];
 
-function starterSnake() {
+(function starterSnake() {
   for (let i = 0; i < startingSnakeSize.length; i++) {
     cellArr[i].classList.add("snake");
   }
-}
-starterSnake();
+})();
 
 function initialMove() {
-  let delay = 500;
   intervalID = setInterval(() => {
     let popValue = [...startingSnakeSize].pop();
     let shiftValue = startingSnakeSize.shift();
     startingSnakeSize.push(popValue + 1);
     cellArr[shiftValue].classList.remove("snake");
-    if ([...startingSnakeSize].pop() === 14) {
-      startingSnakeSize.pop();
-      startingSnakeSize.push(0);
-      startingSnakeSize.forEach((n) => {
-        cellArr[n].classList.add("snake");
-      });
-      return;
-    }
+    // if ([[...startingSnakeSize].pop()].includes(15)) {
+    //   startingSnakeSize.pop();
+    //   startingSnakeSize.push(0);
+    //   startingSnakeSize.forEach((n) => {
+    //     cellArr[n].classList.add("snake");
+    //   });
+    //   return;
+    // }
     startingSnakeSize.forEach((n) => {
       cellArr[n].classList.add("snake");
     });
@@ -40,13 +39,22 @@ function initialMove() {
 initialMove();
 
 document.addEventListener("keydown", (e) => {
+  let downID;
   if (e.key === "ArrowDown") {
     clearInterval(intervalID);
     for (let i = 0; i < startingSnakeSize.length; i++) {
       cellArr[startingSnakeSize[i]].classList.remove("snake");
     }
-    startingSnakeSize = startingSnakeSize.map((n) => {
-      return n + 10;
+    startingSnakeSize = [...startingSnakeSize].map((n, index) => {
+      //   downID = setInterval(() => {
+      //     if (index === startingSnakeSize.length - 1) {
+      //       console.log("yes");
+      //       clearInterval(downID);
+      //       return n + 15;
+      //     }
+      //     return n + 15;
+      //   }, delay);
+      return n + 15;
     });
     initialMove();
   }
