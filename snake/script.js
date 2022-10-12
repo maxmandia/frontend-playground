38aca28626d5bc19263acb5f70c81221efa3947f
Max Mandia <mmandia511@gmail.com>
2022-10-12 17:45:40 -0400

    saving snake work

--- snake/script.js
+++ snake/script.js
@@ -5,7 +5,7 @@ let intervalID;
 
 (function assignRandomCell() {
   let randomCell = Math.floor(Math.random() * cellArr.length);
-  cellArr[randomCell].classList.add("highlight");
+  cellArr[randomCell].innerHTML = "🦦";
 })();
 
 let startingSnakeSize = [0, 1, 2];
@@ -14,43 +14,40 @@ function starterSnake() {
   for (let i = 0; i < startingSnakeSize.length; i++) {
     cellArr[i].classList.add("snake");
   }
-
-  function initialMove() {
-    let delay = 2000;
-    intervalID = setInterval(() => {
-      let popValue = [...startingSnakeSize].pop();
-      let shiftValue = startingSnakeSize.shift();
-      startingSnakeSize.push(popValue + 1);
-      cellArr[shiftValue].classList.remove("snake");
-      console.log(startingSnakeSize);
-      if ([...startingSnakeSize].pop() === 14) {
-        // startingSnakeSize.forEach((n) => {
-        //   cellArr[n].classList.remove("snake");
-        // });
-        startingSnakeSize.pop();
-        startingSnakeSize.push(0);
-        // startingSnakeSize = [0, 1, 2];
-        return;
-      }
-      startingSnakeSize.forEach((n) => {
-        cellArr[n].classList.add("snake");
-      });
-    }, delay);
-  }
-  initialMove();
 }
 starterSnake();
 
-function controlSnake() {}
+function initialMove() {
+  let delay = 500;
+  intervalID = setInterval(() => {
+    let popValue = [...startingSnakeSize].pop();
+    let shiftValue = startingSnakeSize.shift();
+    startingSnakeSize.push(popValue + 1);
+    cellArr[shiftValue].classList.remove("snake");
+    if ([...startingSnakeSize].pop() === 14) {
+      startingSnakeSize.pop();
+      startingSnakeSize.push(0);
+      startingSnakeSize.forEach((n) => {
+        cellArr[n].classList.add("snake");
+      });
+      return;
+    }
+    startingSnakeSize.forEach((n) => {
+      cellArr[n].classList.add("snake");
+    });
+  }, delay);
+}
+initialMove();
 
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowDown") {
     clearInterval(intervalID);
-    console.log(startingSnakeSize.length);
     for (let i = 0; i < startingSnakeSize.length; i++) {
       cellArr[startingSnakeSize[i]].classList.remove("snake");
     }
-    startingSnakeSize = [0, 1, 2];
-    starterSnake();
+    startingSnakeSize = startingSnakeSize.map((n) => {
+      return n + 10;
+    });
+    initialMove();
   }
 });
